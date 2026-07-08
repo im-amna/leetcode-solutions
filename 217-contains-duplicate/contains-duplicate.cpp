47da52c0189bb093472f2f57c21f1af926dddec0
Amna Naveed <amna74345@gmail.com>
2026-07-08 15:59:21 +0500

    Time: 68 ms (25.59%) | Memory: 111.4 MB (5.38%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,20 @@
+#include <unordered_set>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_set<int> st;
+
+        for (int num : nums) {
+
+            if (st.find(num) != st.end()) {
+                return true;
+            }
+
+            st.insert(num);
+        }
+
+        return false;
+    }
+};
